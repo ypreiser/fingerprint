ef75509363e0c0308b9848173403960dd9ae13f2
barzik <ranbz@soluto.com>
2025-01-28 12:17:05 +0200

    88

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import jwt from 'jsonwebtoken';
 import crypto from 'crypto';
 
 const app = express();
-//sign18
+//sign89
 // Middleware setup with more secure CORS options
 app.use(cors({
   origin: process.env.ALLOWED_ORIGINS?.split(',') || 'http://localhost:3000',
